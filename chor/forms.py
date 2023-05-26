88bce3f548790f83b786814a54b09e08225d17b6
@CheParx64 <chayan.par@gmail.com>
2023-05-26 21:55:45 +0200

    Dynamic song creation form developed

--- chor/forms.py
+++ chor/forms.py
@@ -1,15 +1,28 @@
-from django.forms import ModelForm
+from typing import Any, Mapping, Optional, Type, Union
+from django.forms import ModelForm, Form
+from django import forms
+from django.forms.utils import ErrorList
 from .models import Song, SongPropertyName, SongPropertyValue
+
 
 class SongForm(ModelForm):
     class Meta:
         model = Song
-        fields = '__all__'
+        fields = ['name']
+
+
+def SongCreationFormConstructor(fieldnames: list[str]):
+    attr = {'name': forms.CharField(label='Name', max_length=255)}
+    for fieldname in fieldnames:
+        attr.update({fieldname: forms.CharField(label=fieldname, max_length=255, required=False)})
+    return type('SongCreationForm', (Form,), attr)
+
 
 class SongPropertyNameForm(ModelForm):
     class Meta:
         model = SongPropertyName
         fields = '__all__'
+
 
 class SongPropertyValueForm(ModelForm):
     class Meta:
--- chor/views.py
+++ chor/views.py
@@ -1,10 +1,7 @@
-# from .cmbot_songs import SongList
-# from .cmbot_performances import PerformanceList
 from django.shortcuts import render, redirect
-from django.db.models import Count
+from django.db.models import Count, Q
 from .models import Chor, Song, SongPropertyValue, SongPropertyName, SongPerformance
-from .forms import SongForm, SongPropertyNameForm, SongPropertyValueForm
-import datetime
+from .forms import SongForm, SongPropertyNameForm, SongPropertyValueForm, SongCreationFormConstructor
 
 
 def chorhomepage(request, chor_id):
@@ -15,27 +12,75 @@ def chorhomepage(request, chor_id):
 
 def chorsongs(request, chor_id):
     chor = Chor.objects.get(id=chor_id)
-    songs = Song.objects.filter(chor_id=chor_id).annotate(numperformances=Count('songperformance')).order_by('-numperformances')
-    context = {'songs': songs, 'chor': chor}
+    sortby = request.GET.get('s')
+    if sortby:
+        if 'numperformances' in sortby:
+            songs = chor.song_set.all()\
+                .annotate(numperformances=Count('songperformance'))\
+                .order_by(sortby)
+            sortby = None
+        else:
+            if sortby[0] == '-':
+                sortq = '-value'
+                sortby = sortby.lstrip('-')
+            else:
+                sortq = 'value'
+            songs = SongPropertyValue.objects\
+                .filter(Q(song__chor__id=chor_id) & Q(songpropertyname__name=sortby))\
+                .values("song__id", "song__name", 'value')\
+                .order_by(sortq, 'song__name')
+            sortby = sortby.capitalize()
+    else:
+        songs = chor.song_set.all()\
+            .annotate(numperformances=Count('songperformance'))\
+            .order_by('name')
+    attr = SongPropertyName.objects.filter(chor_id=chor_id)
+    context = {'songs': songs, 'chor': chor, 'attributes': attr, 'sortby': sortby}
     return render(request, 'chor/chor-songs.html', context)
 
 def song(request, pk):
     song = Song.objects.get(id=pk)
-    attributes = SongPropertyValue.objects.filter(song_id=pk)
-    performances = SongPerformance.objects.filter(song_id=pk)
+    attributes = song.songpropertyvalue_set.all()
+    performances = song.songperformance_set.all()
     context = {'song': song, 'attributes': attributes, 'performances':performances}
     return render(request, 'chor/song.html', context)
 
-def createSong(request):
-    form = SongForm()
+def createSong(request, chor_id):
+    chor = Chor.objects.get(id=chor_id)
+
+    attrnames = chor.songpropertyname_set.all()
+    attrlist = list()
+    for attr in attrnames:
+        attrlist.append(attr.name)
+    
+    SongCreationForm = SongCreationFormConstructor(attrlist)
+    form = SongCreationForm()
 
     if request.method == "POST":
-        form = SongForm(request.POST)
+        form = SongCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('chor-homepage')
-    
-    context = {'form': form}
+            song = Song.objects.create(
+                chor_id=chor_id,
+                name=form.cleaned_data.get('name')
+            )
+            new_attr_values = list()
+            for attr in attrnames:
+                new_value = form.cleaned_data.get(attr.name)
+                if new_value:
+                    spv = SongPropertyValue.objects.create(
+                        songpropertyname=attr,
+                        song=song,
+                        value=new_value,
+                    )
+                    new_attr_values.append(spv)
+            if song:
+                song.save()
+                for val in new_attr_values:
+                    val.save()
+                return redirect('chor-homepage', chor_id=chor_id)
+    context = {
+        'form': form,
+    }
     return render(request, 'chor/song_form.html', context)
 
 
@@ -54,54 +99,15 @@ def updateSong(request, pk):
 
 def deleteSong(request, pk):
     song = Song.objects.get(id=pk)
+    chor_id = song.chor.id
     if request.method == "POST":
         song.delete()
-        return redirect('home')
-    context = {'obj': song}
+        return redirect('chor-songs', chor_id=chor_id)
+    context = {'song': song}
     return render(request, 'chor/delete.html', context)
-
-# def importSonglist(request):
-#     sl = SongList('chor/songlist.json').json_ready()
-    
-#     out_list = list()
-#     context = dict()
-    
-#     for songo in sl.values():
-#         if songo['status'] != 'deleted':
-#             song_match = Song.objects.get(name=songo['name'])
-#             for key, value in songo.items():
-#                 if not key in ('status', 'name'):
-#                     value_match = SongPropertyName.objects.get(name=key)
-#                     form = SongPropertyValueForm({'song': song_match.id, 'songpropertyname': value_match.id, 'value': value})
-#                     if form.is_valid():
-#                         form.save()
-#                         out_list.append(f"Для песни: '{song_match}' свойству '{value_match}' присвоено значение '{value}'")
-#                     else:
-#                         out_list.append(f"{song_match.id} - {value_match.id} - {value}")
-    
-#     context.update({'out_list': out_list})
-#     return render(request, 'chor/import.html', context)
-
-# def importPerformances(request):
-#     sl = SongList('chor/songlist.json').json_ready()
-#     pl = PerformanceList()
-#     pl.read_from_file('chor/performances.json')
-#     out_list = list()
-#     for item in pl.json_ready():
-#         songo = Song.objects.get(name=sl[item['song_id']].get('name'))
-#         dt = datetime.datetime(*map(int, item['date'].split('-')))
-#         out_list.append(str(songo) + str(dt))
-#         SongPerformance.objects.create(song=songo, datetime=dt)
-#     context = {'out_list': out_list}
-#     return render(request, 'chor/import.html', context)
 
 def chorPerformances(request, chor_id):
     chor = Chor.objects.get(id=chor_id)
-    # songs = Song.objects.filter(chor=chor).select_related('chor')
     performances = SongPerformance.objects.filter(song__chor=chor).order_by('-datetime')
-    # perf = performances.filter(chor_id=chor_id)
-    # for p in performances:
-    #     print(p.__dir__())
-    #     break
     context = {'chor': chor, 'performances': performances}
     return render(request, 'chor/chor-performances.html', context)
