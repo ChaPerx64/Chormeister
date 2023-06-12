f6a1699998edfc3475f3a5c56de9aced9c10ea57
@CheParx64 <chayan.par@gmail.com>
2023-06-12 12:27:50 +0200

    added CD workflow

---
+++ pull-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git pull origin main
+source venv/bin/activate
+pip install -r requirements.txt
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 source venv/bin/activate
-source .DJANGO_SECRET_KEY
 mkdir -pv /var/{log,run}/gunicorn/
 gunicorn -c config/gunicorn/dev.py
 systemctl restart nginx
