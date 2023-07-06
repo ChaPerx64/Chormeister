13ac18b5a248f825e13b7456de2180fc2235dea8
@CheParx64 <chayan.par@gmail.com>
2023-07-06 19:31:37 +0200

    migration completed

--- cmdjango/settings.py
+++ cmdjango/settings.py
@@ -42,14 +42,14 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'user.apps.UserConfig',
+    'chor.apps.ChorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chor.apps.ChorConfig',
-    'user.apps.UserConfig',
     'rest_framework',
 ]
 
--- user/models.py
+++ user/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+# from chor.models import User
 from uuid import uuid4
 
 class ChorRole(models.Model):
